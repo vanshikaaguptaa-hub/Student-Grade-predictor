72bbd44ca0c8cd4b7907b4b9faec96562dad6401
vanshikaaguptaa-hub <vanshika.gupta2_cs25@gla.ac.in>
2026-06-23 00:08:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,14 +13,18 @@ previous_grade = st.slider("Previous Grade", 0, 100, 75)
 extra = st.slider("Extracurricular Activities", 0, 5, 1)
 
 if st.button("Predict Grade"):
-
     data = pd.DataFrame({
-        "AttendanceRate": [attendance],
-        "StudyHoursPerWeek": [study_hours_week],
-        "PreviousGrade": [previous_grade],
-        "ExtracurricularActivities": [extra]
-    })
-
+    "AttendanceRate": [attendance],
+    "StudyHoursPerWeek": [study_hours_week],
+    "PreviousGrade": [previous_grade],
+    "ExtracurricularActivities": [extra],
+    "Study Hours": [3.0],
+    "Attendance (%)": [80],
+    "Gender_Male": [1],
+    "ParentalSupport_Low": [0],
+    "ParentalSupport_Medium": [1],
+    "Online Classes Taken_True": [1]
+})
     prediction = model.predict(data)
 
     st.success(f"Predicted Final Grade: {prediction[0]:.2f}")
